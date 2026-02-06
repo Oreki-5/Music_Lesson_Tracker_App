12a4ac76742c7c09508d576d9ad7142192e6044d
Aditya Hindlekar <adityacoc1602@example.com>
2026-02-06 16:24:21 +0530

    first commit

---
+++ src/test/java/com/Oreki/Music_Tracker/MusicTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oreki.Music_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
