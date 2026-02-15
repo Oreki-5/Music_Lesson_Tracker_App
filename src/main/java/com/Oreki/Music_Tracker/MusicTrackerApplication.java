a953724e39946c50ae7da81439b2f5f069e81b70
Oreki-5 <adityacoc1602@gmail.com>
2026-02-15 12:18:59 +0530

    finally db setup complete

--- src/main/java/com/Oreki/Music_Tracker/MusicTrackerApplication.java
+++ src/main/java/com/Oreki/Music_Tracker/MusicTrackerApplication.java
@@ -1,9 +1,11 @@
 package com.Oreki.Music_Tracker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages= "com.Oreki.Music_Tracker")
+@EnableAutoConfiguration
 public class MusicTrackerApplication {
 
 	public static void main(String[] args) {
